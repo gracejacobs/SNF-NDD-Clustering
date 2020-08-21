31cb8a78a3217a4025c78dde6702762a1cae62e1
Grace <grace.jacobs17@gmail.com>
2020-08-21 18:17:23 -0400

    classifier

---
+++ code/5_Supervised_classification/Running_RF_classifier.r
@@ -0,0 +1,399 @@
+## Running a random forest classifer on POND SNF groups based on subsets of features
+#1) All features
+#2) Top 2 features from each data type
+#3) Top 35 features
+#4) Top 10 features
+
+library(randomForest)
+library(caret)
+library(data.table)
+
+directory <- ("output/5_Supervised_classification")
+
+## inporting cluster labels
+labels <- read.csv("output/1_SNF_analysis/4clust_groups_k18_0.8_1000perms.csv")
+labels$groups_2 <- ifelse(labels$groups == "1", "2", NA)
+labels$groups_2 <- ifelse(labels$groups == "2", "4", labels$groups_2)
+labels$groups_2 <- ifelse(labels$groups == "3", "3", labels$groups_2)
+labels$groups_2 <- ifelse(labels$groups == "4", "1", labels$groups_2)
+labels$groups <- labels$groups_2
+labels <- as.data.frame(labels$groups)
+
+ids <- read.csv("data/Data_with_headers/ids.csv") # participant subject identifiers
+measures <- read.csv("data/Data_with_headers/all_measures.csv")
+measures <- measures[, c(2:129, 198:204)]
+
+## gets the resampling subgroups 
+source("/projects/gjacobs/POND/machine_classifier/setup_classifer_script.R")
+
+numboot <- 100 # number of permutations 
+nsub <- 176 #number of subjects
+bootsize <- 0.8 #what percentage of participants to take per permuation
+
+## getting the testing and training set
+both <- dividing_data(numboot=numboot, nsub=nsub, bootsize=bootsize, labels=labels, measures=measures, ids=ids)
+train <- both[1:numboot, ]
+test <- both[(1+numboot):(2*numboot), ]
+
+###########################################################################################
+## Choosing which features will be included in the model
+
+# All features included in the classification model
+
+names <- read.csv("output/1_SNF_analysis/all_scores_k18_0.8_1000perms.csv", header = TRUE)
+names <- names[1:135, 3]
+#adjusting tract names
+names <- gsub('-', '.', names)
+names <- gsub('/', '.', names)
+#setdiff(colnames(measures), names)
+names <- as.data.frame(names)
+
+measures <- measures[,colnames(measures) %in% names] 
+names(measures)
+# checking measure variance
+nzv <- nearZeroVar(measures, saveMetrics = TRUE)
+sum(nzv$nzv == TRUE)
+
+# adding the cluster labels to the measures
+lm <- cbind(labels, measures)
+# adding the cluster labels to the ids
+lm <- cbind(ids, lm)
+# ordering measures by cluster labels
+lm <- lm[order(lm$`labels$groups`), ]
+lm$x <- NULL
+
+perms <- data.frame(matrix(0, nrow = numboot, ncol = nsub))
+bn <- nsub*bootsize #number of subjects in each bootstrap
+
+##creating matrices to keep track of accuracies
+acc_overall <- data.frame(matrix(0, nrow = 7, ncol = numboot))
+acc_class <- data.frame(matrix(0, nrow = 1, ncol = 11))
+list_adjrandindex <- data.frame(matrix(NA, nrow = numboot, ncol = 1)) 
+importance_features <- data.frame(matrix(NA, nrow = length(measures), ncol = numboot))
+importance_values <- data.frame(matrix(NA, nrow = length(measures), ncol = numboot))
+
+### Running the actual random forest classifer
+for(idx in 1:numboot){
+  print(idx) # permutation number
+  subjects <- t(train[idx, ]) # getting training set subjects for that permutation
+  training_set <- cbind(subjects, lm) # adding cluster labels
+  training_set <- training_set[which(training_set[,1] == "1"),] ## only getting the people for this perm
+  labels_train <- as.factor(training_set[ ,2]) ## recording these labels
+  training_set[,1:2] <- NULL
+  
+  ## setting up testing dataset
+  subjects <- t(test[idx, ]) # getting testing subjects for that permutation
+  testing_set <- cbind(subjects, lm)
+  testing_set <- testing_set[which(testing_set[,1] == "1"),]
+  labels_test <- as.factor(testing_set[ ,2])
+  testing_set[,1:2] <- NULL
+  
+  ## training random forest
+  model.rf = randomForest(labels_train ~. , data=training_set, ntree=501, mtry=80, importance=TRUE)
+
+  #plot(svmfit, training_set)
+  rfpredict <- predict(model.rf, testing_set)
+  plot(rfpredict, labels_test)
+  summary(rfpredict)
+  
+  # calculating the most important features
+  import <- as.data.frame(importance(model.rf, type=1))
+  import$Features <- row.names(import)
+  import <- import[order(-import$MeanDecreaseAccuracy), ]
+  importance_features[ ,idx] <- import$Features
+  importance_values[ ,idx] <- import$MeanDecreaseAccuracy
+  
+  varImpPlot(model.rf, sort = TRUE)
+  
+  ### testing accuracy
+  stats <- confusionMatrix(rfpredict, labels_test) 
+  
+  acc_bygroup_perm <- as.data.frame(stats[4][1])
+  names(acc_class) <- names(acc_bygroup_perm)
+  acc_class <- rbind(acc_class, acc_bygroup_perm)
+  
+  row.names(acc_overall) <- row.names(as.data.frame(stats[3][1]))
+  acc_overall[ , idx] <- as.data.frame(stats[3][1])
+  list_adjrandindex[idx, 1] <- adj.rand.index(rfpredict, labels_test)
+}
+
+model <- "all_mtry80"
+
+write.csv(list_adjrandindex, file=file.path(directory, paste("list_adjrandidx_100perms_", model, ".csv", sep="")))
+write.csv(acc_class, file=file.path(directory, paste("acc_by_group_100perms_", model, ".csv", sep="")))
+write.csv(acc_overall, file=file.path(directory, paste("acc_overall_100perms_", model, ".csv", sep="")))
+
+###########################################################################################
+
+# including top 2 measures from each
+names <- read.csv("output/1_SNF_analysis/all_scores_k18_0.8_1000perms.csv", header = TRUE)
+names <- names[1:135, 3]
+#adjusting tract names
+names <- gsub('-', '.', names)
+names <- gsub('/', '.', names)
+#setdiff(colnames(measures), names)
+names <- as.data.frame(names)
+
+names <- names[which(names$names == "ADHD_I_SUB" | names$names == "ADHD_HI_SUB"| names$names == "R_insula_thickavg"| names$names == "R_parstriangularis_thickavg"
+                     | names$names =="Rpal"| names$names =="Rput"| names$names == "ALIC.L"| names$names == "RLIC.R"),]
+
+measures <- measures[,colnames(measures) %in% names] 
+names(measures)
+# checking measure variance
+nzv <- nearZeroVar(measures, saveMetrics = TRUE)
+sum(nzv$nzv == TRUE)
+
+# adding the cluster labels to the measures
+lm <- cbind(labels, measures)
+# adding the cluster labels to the ids
+lm <- cbind(ids, lm)
+# ordering measures by cluster labels
+lm <- lm[order(lm$`labels$groups`), ]
+lm$x <- NULL
+
+perms <- data.frame(matrix(0, nrow = numboot, ncol = nsub))
+bn <- nsub*bootsize #number of subjects in each bootstrap
+
+##creating matrices to keep track of accuracies
+acc_overall <- data.frame(matrix(0, nrow = 7, ncol = numboot))
+acc_class <- data.frame(matrix(0, nrow = 1, ncol = 11))
+list_adjrandindex <- data.frame(matrix(NA, nrow = numboot, ncol = 1)) 
+importance_features <- data.frame(matrix(NA, nrow = length(measures), ncol = numboot))
+importance_values <- data.frame(matrix(NA, nrow = length(measures), ncol = numboot))
+
+### Running the actual random forest classifer
+for(idx in 1:numboot){
+  print(idx) # permutation number
+  subjects <- t(train[idx, ]) # getting training set subjects for that permutation
+  training_set <- cbind(subjects, lm) # adding cluster labels
+  training_set <- training_set[which(training_set[,1] == "1"),] ## only getting the people for this perm
+  labels_train <- as.factor(training_set[ ,2]) ## recording these labels
+  training_set[,1:2] <- NULL
+  
+  ## setting up testing dataset
+  subjects <- t(test[idx, ]) # getting testing subjects for that permutation
+  testing_set <- cbind(subjects, lm)
+  testing_set <- testing_set[which(testing_set[,1] == "1"),]
+  labels_test <- as.factor(testing_set[ ,2])
+  testing_set[,1:2] <- NULL
+  
+  ## training random forest
+  model.rf = randomForest(labels_train ~. , data=training_set, ntree=501, mtry=4, importance=TRUE)
+  
+  #plot(svmfit, training_set)
+  rfpredict <- predict(model.rf, testing_set)
+  plot(rfpredict, labels_test)
+  summary(rfpredict)
+  
+  # calculating the most important features
+  import <- as.data.frame(importance(model.rf, type=1))
+  import$Features <- row.names(import)
+  import <- import[order(-import$MeanDecreaseAccuracy), ]
+  importance_features[ ,idx] <- import$Features
+  importance_values[ ,idx] <- import$MeanDecreaseAccuracy
+ 
+  varImpPlot(model.rf, sort = TRUE)
+  
+  ### testing accuracy
+  stats <- confusionMatrix(rfpredict, labels_test) 
+  
+  acc_bygroup_perm <- as.data.frame(stats[4][1])
+  names(acc_class) <- names(acc_bygroup_perm)
+  acc_class <- rbind(acc_class, acc_bygroup_perm)
+  
+  row.names(acc_overall) <- row.names(as.data.frame(stats[3][1]))
+  acc_overall[ , idx] <- as.data.frame(stats[3][1])
+  list_adjrandindex[idx, 1] <- adj.rand.index(rfpredict, labels_test)
+}
+
+model <- "top2_mtry4"
+
+write.csv(list_adjrandindex, file=file.path(directory, paste("list_adjrandidx_100perms_", model, ".csv", sep="")))
+write.csv(acc_class, file=file.path(directory, paste("acc_by_group_100perms_", model, ".csv", sep="")))
+write.csv(acc_overall, file=file.path(directory, paste("acc_overall_100perms_", model, ".csv", sep="")))
+
+###########################################################################################
+
+# including top 35 features
+names <- read.csv("output/1_SNF_analysis/all_scores_k18_0.8_1000perms.csv", header = TRUE)
+names <- names[1:135, 3]
+#adjusting tract names
+names <- gsub('-', '.', names)
+names <- gsub('/', '.', names)
+#setdiff(colnames(measures), names)
+names <- as.data.frame(names)
+
+names <- names[1:35, ]
+
+measures <- measures[,colnames(measures) %in% names] 
+names(measures)
+# checking measure variance
+nzv <- nearZeroVar(measures, saveMetrics = TRUE)
+sum(nzv$nzv == TRUE)
+
+# adding the cluster labels to the measures
+lm <- cbind(labels, measures)
+# adding the cluster labels to the ids
+lm <- cbind(ids, lm)
+# ordering measures by cluster labels
+lm <- lm[order(lm$`labels$groups`), ]
+lm$x <- NULL
+
+perms <- data.frame(matrix(0, nrow = numboot, ncol = nsub))
+bn <- nsub*bootsize #number of subjects in each bootstrap
+
+##creating matrices to keep track of accuracies
+acc_overall <- data.frame(matrix(0, nrow = 7, ncol = numboot))
+acc_class <- data.frame(matrix(0, nrow = 1, ncol = 11))
+list_adjrandindex <- data.frame(matrix(NA, nrow = numboot, ncol = 1)) 
+importance_features <- data.frame(matrix(NA, nrow = length(measures), ncol = numboot))
+importance_values <- data.frame(matrix(NA, nrow = length(measures), ncol = numboot))
+
+### Running the actual random forest classifer
+for(idx in 1:numboot){
+  print(idx) # permutation number
+  subjects <- t(train[idx, ]) # getting training set subjects for that permutation
+  training_set <- cbind(subjects, lm) # adding cluster labels
+  training_set <- training_set[which(training_set[,1] == "1"),] ## only getting the people for this perm
+  labels_train <- as.factor(training_set[ ,2]) ## recording these labels
+  training_set[,1:2] <- NULL
+  
+  ## setting up testing dataset
+  subjects <- t(test[idx, ]) # getting testing subjects for that permutation
+  testing_set <- cbind(subjects, lm)
+  testing_set <- testing_set[which(testing_set[,1] == "1"),]
+  labels_test <- as.factor(testing_set[ ,2])
+  testing_set[,1:2] <- NULL
+  
+  ## training random forest
+  model.rf = randomForest(labels_train ~. , data=training_set, ntree=501, mtry=15, importance=TRUE)
+  
+  #plot(svmfit, training_set)
+  rfpredict <- predict(model.rf, testing_set)
+  plot(rfpredict, labels_test)
+  summary(rfpredict)
+  
+  # calculating the most important features
+  import <- as.data.frame(importance(model.rf, type=1))
+  import$Features <- row.names(import)
+  import <- import[order(-import$MeanDecreaseAccuracy), ]
+  importance_features[ ,idx] <- import$Features
+  importance_values[ ,idx] <- import$MeanDecreaseAccuracy
+  
+  varImpPlot(model.rf, sort = TRUE)
+  
+  ### testing accuracy
+  stats <- confusionMatrix(rfpredict, labels_test) 
+  
+  acc_bygroup_perm <- as.data.frame(stats[4][1])
+  names(acc_class) <- names(acc_bygroup_perm)
+  acc_class <- rbind(acc_class, acc_bygroup_perm)
+  
+  row.names(acc_overall) <- row.names(as.data.frame(stats[3][1]))
+  acc_overall[ , idx] <- as.data.frame(stats[3][1])
+  list_adjrandindex[idx, 1] <- adj.rand.index(rfpredict, labels_test)
+}
+
+model <- "top35_15mtry"
+
+write.csv(list_adjrandindex, file=file.path(directory, paste("list_adjrandidx_100perms_", model, ".csv", sep="")))
+write.csv(acc_class, file=file.path(directory, paste("acc_by_group_100perms_", model, ".csv", sep="")))
+write.csv(acc_overall, file=file.path(directory, paste("acc_overall_100perms_", model, ".csv", sep="")))
+
+
+###########################################################################################
+
+# including top 10 features
+names <- read.csv("output/1_SNF_analysis/all_scores_k18_0.8_1000perms.csv", header = TRUE)
+names <- names[1:135, 3]
+#adjusting tract names
+names <- gsub('-', '.', names)
+names <- gsub('/', '.', names)
+#setdiff(colnames(measures), names)
+names <- as.data.frame(names)
+
+names <- names[1:10, ]
+
+measures <- measures[,colnames(measures) %in% names] 
+names(measures)
+# checking measure variance
+nzv <- nearZeroVar(measures, saveMetrics = TRUE)
+sum(nzv$nzv == TRUE)
+
+# adding the cluster labels to the measures
+lm <- cbind(labels, measures)
+# adding the cluster labels to the ids
+lm <- cbind(ids, lm)
+# ordering measures by cluster labels
+lm <- lm[order(lm$`labels$groups`), ]
+lm$x <- NULL
+
+perms <- data.frame(matrix(0, nrow = numboot, ncol = nsub))
+bn <- nsub*bootsize #number of subjects in each bootstrap
+
+##creating matrices to keep track of accuracies
+acc_overall <- data.frame(matrix(0, nrow = 7, ncol = numboot))
+acc_class <- data.frame(matrix(0, nrow = 1, ncol = 11))
+list_adjrandindex <- data.frame(matrix(NA, nrow = numboot, ncol = 1)) 
+importance_features <- data.frame(matrix(NA, nrow = length(measures), ncol = numboot))
+importance_values <- data.frame(matrix(NA, nrow = length(measures), ncol = numboot))
+
+### Running the actual random forest classifer
+for(idx in 1:numboot){
+  print(idx) # permutation number
+  subjects <- t(train[idx, ]) # getting training set subjects for that permutation
+  training_set <- cbind(subjects, lm) # adding cluster labels
+  training_set <- training_set[which(training_set[,1] == "1"),] ## only getting the people for this perm
+  labels_train <- as.factor(training_set[ ,2]) ## recording these labels
+  training_set[,1:2] <- NULL
+  
+  ## setting up testing dataset
+  subjects <- t(test[idx, ]) # getting testing subjects for that permutation
+  testing_set <- cbind(subjects, lm)
+  testing_set <- testing_set[which(testing_set[,1] == "1"),]
+  labels_test <- as.factor(testing_set[ ,2])
+  testing_set[,1:2] <- NULL
+  
+  ## training random forest
+  model.rf = randomForest(labels_train ~. , data=training_set, ntree=501, mtry=5, importance=TRUE)
+  
+  #plot(svmfit, training_set)
+  rfpredict <- predict(model.rf, testing_set)
+  plot(rfpredict, labels_test)
+  summary(rfpredict)
+  
+  # calculating the most important features
+  import <- as.data.frame(importance(model.rf, type=1))
+  import$Features <- row.names(import)
+  import <- import[order(-import$MeanDecreaseAccuracy), ]
+  importance_features[ ,idx] <- import$Features
+  importance_values[ ,idx] <- import$MeanDecreaseAccuracy
+  
+  varImpPlot(model.rf, sort = TRUE)
+  
+  ### testing accuracy
+  stats <- confusionMatrix(rfpredict, labels_test) 
+  
+  acc_bygroup_perm <- as.data.frame(stats[4][1])
+  names(acc_class) <- names(acc_bygroup_perm)
+  acc_class <- rbind(acc_class, acc_bygroup_perm)
+  
+  row.names(acc_overall) <- row.names(as.data.frame(stats[3][1]))
+  acc_overall[ , idx] <- as.data.frame(stats[3][1])
+  list_adjrandindex[idx, 1] <- adj.rand.index(rfpredict, labels_test)
+}
+
+model <- "top10_mtry5"
+
+write.csv(list_adjrandindex, file=file.path(directory, paste("list_adjrandidx_100perms_", model, ".csv", sep="")))
+write.csv(acc_class, file=file.path(directory, paste("acc_by_group_100perms_", model, ".csv", sep="")))
+write.csv(acc_overall, file=file.path(directory, paste("acc_overall_100perms_", model, ".csv", sep="")))
+
+
+
+
+
+
+
+
+
---
+++ code/5_Supervised_classification/setup_classifer_script.R
@@ -0,0 +1,64 @@
+### Creates function for random forest classifier
+## divides participants into an 80/20 train test split
+library(e1071)
+library(rpart)
+library(fossil)
+library(caret)
+
+# function to divide the data into training and testing samples across permutations
+dividing_data <- function(numboot, nsub, bootsize, group1size, group2size, group3size, group4size){
+  
+  perms <- data.frame(matrix(0, nrow = numboot, ncol = nsub))
+  bn <- nsub*bootsize #number of subjects in each bootstrap
+
+  train <- data.frame(matrix(0, nrow = numboot, ncol = nsub))
+  testing <- data.frame(matrix(0, nrow = numboot, ncol = nsub))
+  testing_labels <- data.frame(matrix(0, nrow = numboot, ncol = nsub))
+
+  print("1. Creating matrices of which participants to include for training and testing (80%)")
+  for(idx in seq(1:numboot)){
+    print(idx)
+    test <- 0
+    while(test == "0"){
+      test <- 1
+      rnd_1 <- sample(1:nsub, bn)
+      
+      inc <- data.frame(matrix(0, nrow = 1, ncol = nsub)) # row of all subjects to determine which ones are included in this clustering
+      # if the column of inc is not included in rnd, then it will be equal to 0
+      for (i in 1:ncol(inc)){
+        #inc[1,i] <- ifelse(i %in% rnd_1 | i %in% rnd_2 | i %in% rnd_3 | i %in% rnd_4, 1, 0)
+        inc[1,i] <- ifelse(i %in% rnd_1, 1, 0)
+      }
+      
+      # checking to see if inc is the same as any of the other perms
+      bad <- 0 
+      for (row in 1:nrow(train)){
+        bad <- ifelse(identical(perms[row, ], inc[1, ]), bad + 1, bad)
+      } 
+      
+      if (bad > 0){ # aka, this is the same clustering solution as any of the previous ones
+        test <- 0
+      } else {
+        test <- 1
+      }
+    }
+    
+    for (i in 1:ncol(inc)){ # adding the row of subjects for the permutation to the permutation matrix
+      train[idx,i] <- inc[1,i]
+    }
+    
+    ## Setting up testing matrix
+    testing_peeps <- data.frame(matrix(0, nrow = 1, ncol = nsub))
+    for (i in 1:ncol(inc)){
+      testing_peeps[1,i] <- ifelse(inc[1,i] == "0", 1, 0)
+    }
+    for (i in 1:ncol(testing_peeps)){ # adding the row of subjects for the permutation to the permutation matrix
+      testing[idx,i] <- testing_peeps[1,i]
+    }
+  } 
+  # putting both together so that I can return them
+  both <- rbind(train, testing)
+  return(both)
+}
+################################################################################################################################
+################################################################################################################################
