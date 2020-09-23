94ac982a074b36e9dd515d4e42edd4e2bbe30981
gracejacobs <grace.jacobs17@gmail.com>
2020-09-23 15:04:02 -0400

    Update setting_up_data_for_cluster_analysis.r

--- code/2_Comparing_subgroups/setting_up_data_for_cluster_analysis.r
+++ code/2_Comparing_subgroups/setting_up_data_for_cluster_analysis.r
@@ -1,36 +1,38 @@
-### Setting up data for the analysis of SNF clusters
+### Setting up data for the analysis and comparison of SNF clusters
 
 clusters <- read.csv("output/1_SNF_analysis/4clust_groups_k18_0.8_1000perms.csv")
-
+# reordering based on highest to lowerest impairment (determined after cluster comparisons)
 clusters$groups_2 <- ifelse(clusters$groups == "1", "2", NA)
 clusters$groups_2 <- ifelse(clusters$groups == "2", "4", clusters$groups_2)
 clusters$groups_2 <- ifelse(clusters$groups == "3", "3", clusters$groups_2)
 clusters$groups_2 <- ifelse(clusters$groups == "4", "1", clusters$groups_2)
 
 clusters$groups <- clusters$groups_2
-
 clusters$X <- NULL
 clusters$id <- NULL
 names(clusters)[names(clusters)=="V1"] <- "ID"
 
-# data with headers
+# importing individual data type matrices with measure headers
 directory <- ("data/Data_with_headers")
 CT <-read.csv(file.path(directory, paste("CT_snf.csv", sep="")))
 SA <-read.csv(file.path(directory, paste("SA_snf.csv", sep="")))
 Volume <-read.csv(file.path(directory, paste("Volumes_snf.csv", sep="")))
 FA <-read.csv(file.path(directory, paste("FA_snf.csv", sep="")))
 ids <-read.csv(file.path(directory, paste("ids.csv", sep="")))
-
 names(ids)[names(ids)=="x"] <- "ID"
+
+# importing clinical data
 pond <- read_sas("data/Clinical_cog_data/pond_extract_06jun2018.sas7bdat")
 pond$SUBJECT <- ifelse(grepl("^88",pond$SUBJECT), paste("sub-0", pond$SUBJECT, sep=""), paste("sub-", pond$SUBJECT, sep=""))
 names(pond)[names(pond) == "SUBJECT"] <- "ID"
+# importing additional social communication data
 scq1 <- read.csv("data/Clinical_cog_data/SCQ Data 13 Aug 2018.csv")
 scq1$Subject <- ifelse(grepl("^88",scq1$Subject), paste("sub-0", scq1$Subject, sep=""), paste("sub-", scq1$Subject, sep=""))
 names(scq1)[names(scq1) == "Subject"] <- "ID"
 scq1 <- scq1[,c("ID", "SCQTOT")]
 pond <- merge(pond, scq1, by="ID")
 
+# calculating age
 pond$DOB_2 <- gsub('\\-', '.', pond$DOB)
 pond$POND_DATE_2 <- gsub('\\-', '.', pond$POND_DATE)
 pond$DOB_2 <- strptime(pond$DOB_2, format = "%Y.%m.%d")
@@ -41,6 +43,7 @@ pond$DOB_2 <- NULL
 pond$POND_DATE_2 <- NULL
 pond$age <- as.numeric(pond$age)
 
+# defining clinical diagnoses
 pond$clin_diagnosis <- ifelse(pond$RESEARCH_CONFIRM_DIAG_STD == "1", "ASD", NA)
 pond$clin_diagnosis <- ifelse(pond$RESEARCH_CONFIRM_DIAG_STD == "2", "ADHD", pond$clin_diagnosis)
 pond$clin_diagnosis <- ifelse(pond$RESEARCH_CONFIRM_DIAG_STD == "3", "OCD", pond$clin_diagnosis)
@@ -48,25 +51,26 @@ pond$clin_diagnosis <- ifelse(pond$RESEARCH_CONFIRM_DIAG_STD == "6", "ADHD", pon
 pond$clin_diagnosis <- ifelse(pond$RESEARCH_CONFIRM_DIAG_STD == "10", "CTRL", pond$clin_diagnosis)
 pond$clin_diagnosis <- ifelse(pond$RESEARCH_CONFIRM_DIAG_STD == "15", "GAD", pond$clin_diagnosis)
 
+# keeping only data for participants included in the study
 all <- merge(pond, ids, by="ID")
-
 all$clin_diagnosis <- factor(all$clin_diagnosis, labels=c("ADHD", "ASD", "OCD"))
 table(all$clin_diagnosis)
 
+# merging together all the data
 all <- merge(clusters, all, by="ID")
-
 all <- cbind(all, CT)
 all <- cbind(all, FA)
 all <- cbind(all, Volume)
 all <- cbind(all, SA)
 
+# organizing IQ measures from different scales
 all$IQ <- all$WASI_FSIQ_4
 all$IQ <- ifelse(is.na(all$IQ), all$WASI_II_FSIQ_4, all$IQ)
 all$IQ <- ifelse(is.na(all$IQ), all$WISC_IV_FSIQ, all$IQ)
 all$IQ <- ifelse(is.na(all$IQ), all$WISC_V_FSIQ, all$IQ)
 all$IQ <- ifelse(is.na(all$IQ), all$WASI_II_FSIQ_2, all$IQ)
 
-
+# ensuring classes of variables are correct
 all$GENDER <- as.factor(all$GENDER)
 all$group <- as.factor(all$groups)
 all$age <- as.numeric(all$age)
@@ -77,9 +81,9 @@ clust2 <- all[which(all$group=="2"),]
 clust3 <- all[which(all$group=="3"),]
 clust4 <- all[which(all$group=="4"),]
 
-
 all$intel <- all$IQ
 
+# reorganizing measures into stacks to make certain analyses easier
 top_measures <- all %>%
   select(ID, clin_diagnosis, group, group_sex, age, GENDER, R_parstriangularis_thickavg, R_insula_thickavg, ADHD_I_SUB, R_middletemporal_thickavg, L_supramarginal_thickavg, R_inferiortemporal_thickavg, L_middletemporal_thickavg, R_superiorfrontal_thickavg, L_rostralmiddlefrontal_thickavg, L_insula_thickavg, L_fusiform_thickavg, L_parsopercularis_thickavg, 
          R_inferiorparietal_thickavg, R_rostralmiddlefrontal_thickavg, L_inferiorparietal_thickavg, L_inferiortemporal_thickavg, R_precentral_thickavg, L_lateralorbitofrontal_thickavg,
